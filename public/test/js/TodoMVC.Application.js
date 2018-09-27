67d3b2bb5b029be2daa31c9b50557a6f925c80c9
Benjamin Adams <armastevs@gmail.com>
2018-09-27 12:22:33 -0500

    init ui stuff

---
+++ public/test/js/TodoMVC.Application.js
@@ -0,0 +1,31 @@
+/*global Backbone, TodoMVC:true */
+
+var TodoMVC = TodoMVC || {};
+
+(function () {
+	'use strict';
+
+
+
+
+	window.likedPets = localStorage.getItem("likedpets") || []
+
+
+
+	//localStorage.setItem("key", "value");
+
+
+
+	var TodoApp = Mn.Application.extend({
+		setRootLayout: function () {
+			this.root = new TodoMVC.RootLayout();
+		}
+	});
+
+	TodoMVC.App = new TodoApp();
+
+	TodoMVC.App.on('before:start', function () {
+		TodoMVC.App.setRootLayout();
+	});
+
+})();
